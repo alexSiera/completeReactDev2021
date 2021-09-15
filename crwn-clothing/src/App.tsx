e8d0bbdb55fc09714f93b1eafc37deb72a0d4d9c
Alexander Pavlovtsev <alexander.pavlovtsev@fusion-tech.pro>
2021-09-15 15:48:33 +0300

    End up 58 lesson

--- crwn-clothing/src/App.tsx
+++ crwn-clothing/src/App.tsx
@@ -1,11 +1,14 @@
+import Header from 'components/header/header.component';
 import HomePage from 'pages/homepage/homepage.component';
 import ShopPage from 'pages/shop/shop.component';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 
+
 const App = () => (
 	<div>
+    <Header />
 		<Switch>
 			<Route exact path='/' component={HomePage} />
 			<Route path='/shop' component={ShopPage} />
